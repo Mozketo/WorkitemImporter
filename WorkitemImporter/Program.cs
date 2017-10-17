9df8a942436cbcc8184181b95c7dbce17f85787b
Ben Clark-Robinson <mozketo@live.com>
2017-10-17 17:47:00 +1300

    Attempting to fetch work items

--- WorkitemImporter/Program.cs
+++ WorkitemImporter/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using NDesk.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,81 @@ namespace WorkitemImporter
     {
         static void Main(string[] args)
         {
+            bool showHelp = false;
+            var vstsConfig = new VstsConfig();
+            var jiraConfig = new JiraConfig();
+
+            var p = new OptionSet() {
+                { "vsts-url=", "VSTS URL", v => vstsConfig.Url = v},
+                { "vsts-project=", "VSTS project name", v => vstsConfig.Project = v},
+                { "vsts-pat=",  "VSTS Personal Access Token", v => vstsConfig.PersonalAccessToken = v },
+                { "jira-url=", "Jira URL", v => jiraConfig.Url = v},
+                { "jira-userid=",  "Jira User ID", v => jiraConfig.UserId = v },
+                { "vsts-password=", "VSTS URL", v => jiraConfig.Password = v},
+                { "h|help",  "show this message and exit", v => showHelp = v != null },
+            };
+
+            List<string> extra;
+            try
+            {
+                extra = p.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write($"WorkitemImporter {Assembly.GetExecutingAssembly().GetName().Version}: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `myconsole --help' for more information.");
+                return;
+            }
+
+            if (showHelp)
+            {
+                ShowHelp(p);
+                return;
+            }
+        }
+
+        static void ShowHelp(OptionSet p)
+        {
+            Console.WriteLine("Usage: WorkitemImporter [OPTIONS]");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            p.WriteOptionDescriptions(Console.Out);
+        }
+    }
+
+    public sealed class VstsConfig
+    {
+        string url;
+        public string Url
+        {
+            get { return url; }
+            set
+            {
+                url = value.StartsWith("http", StringComparison.OrdinalIgnoreCase)
+                    ? value
+                    : $"https://{value}.visualstudio.com";
+            }
+        }
+
+        public string PersonalAccessToken { get; set; }
+        public string Project { get; set; }
+    }
+
+    public sealed class JiraConfig
+    {
+        public string UserId { get; set; }
+        public string Password { get; set; }
+        string url;
+        public string Url
+        {
+            get { return url; }
+            set
+            {
+                url = value.StartsWith("http", StringComparison.OrdinalIgnoreCase)
+                    ? value
+                    : $"https://{value}.atlassian.net";
+            }
         }
     }
 }
---
+++ WorkitemImporter/Sync.cs
@@ -0,0 +1,38 @@
+﻿using Atlassian.Jira;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkitemImporter
+{
+    sealed class Sync
+    {
+        public VstsConfig VstsConfig { get; }
+        public JiraConfig JiraConfig { get; }
+
+        public Sync(VstsConfig vstsConfig, JiraConfig jiraConfig)
+        {
+            JiraConfig = jiraConfig;
+            VstsConfig = vstsConfig;
+        }
+
+        public void Process()
+        {
+            //string vstsUrl = $"https://{VstsConfig.Url}.visualstudio.com";
+            var connection = new VssConnection(new Uri(VstsConfig.Url), new VssBasicCredential(string.Empty, VstsConfig.PersonalAccessToken));
+            var witClient = connection.GetClient<WorkItemTrackingHttpClient>();
+
+            var jiraConn = Jira.CreateRestClient(JiraConfig.Url, JiraConfig.UserId, JiraConfig.Password);
+            var issues = (from i in jiraConn.Issues.Queryable orderby i.Created select i).ToList();
+
+            foreach (var issue in issues)
+            {
+                var all = witClient.GetClassificationNodeAsync(VstsConfig.Project, TreeStructureGroup.Iterations, null, 10).Result;
+            }
+        }
+    }
+}
